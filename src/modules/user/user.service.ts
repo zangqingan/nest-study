a2432062d54dd7b0662be9f22e1273dc191bb4a3
wanggeng <18367319791@163.com>
2023-08-11 16:59:55 +0800

    feat:注册用户判断

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -14,23 +14,32 @@ export class UserService {
     private jwtService: JwtService,
   ) {}
 
+  /**
+   *
+   * @param createUserDto 用户信息
+   * @returns 新注册用户信息
+   */
   async create(createUserDto: CreateUserDto) {
     const { username } = createUserDto;
-    // 是否已存在用户
+    // 判断是否已存在用户
     const isExist = await this.userRepository.findOne({
       where: { username },
     });
+    console.log('isExist', isExist);
     if (isExist) {
       throw new HttpException('用户名已存在', HttpStatus.BAD_REQUEST);
     }
-    // 写入数据库
-    const newUser = await this.userRepository.create(createUserDto);
-    // 返回
-    return await this.userRepository.save(newUser);
+    try {
+      // 写入数据库
+      const newUser = await this.userRepository.create(createUserDto);
+      // 返回
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async findAll() {
-    throw new HttpException('查找失败', HttpStatus.NOT_FOUND);
     const result = await this.userRepository.find();
     return `This action returns all user${result}`;
   }
